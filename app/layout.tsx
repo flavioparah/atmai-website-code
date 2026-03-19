cc09ba5de3bc9405e85ba1f8b4cbdc2fa0b6de13
Flávio Parah <67834964+flavioparah@users.noreply.github.com>
2026-03-19 00:27:35 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,10 @@ export const metadata: Metadata = {
   generator: 'v0.app',
   icons: {
     icon: [
+       {
+        url: '/icon.svg',
+        type: 'image/svg+xml',
+      },
       {
         url: '/icon-light-32x32.png',
         media: '(prefers-color-scheme: light)',
@@ -20,10 +24,6 @@ export const metadata: Metadata = {
       {
         url: '/icon-dark-32x32.png',
         media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
       },
     ],
     apple: '/apple-icon.png',
